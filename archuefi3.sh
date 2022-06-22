72931e33d72c697f43d02ed2a8fdbee5e1f4966f
Technomag31 <66881092+Technomag31@users.noreply.github.com>
2022-06-22 19:13:22 +0200

    Add files via upload

--- archuefi3.sh
+++ archuefi3.sh
@@ -22,12 +22,40 @@ sudo '/home/technomag31/swapfile                                 none           
 echo 'programs'
 sudo pacman -S telegram-desktop krita pulseaudio pavucontrol transmission-cli virtualbox teamviewer steam --noconfirm
 
+
 cd ~/AUR
 git clone https://aur.archlinux.org/google-chrome.git 
 cd google-chrome 
-Makepkg -si
-sudo rm -r google-chrome
+sudo makepkg -si --noconfirm
+cd ~/AUR
+sudo rm -r cd ~/AUR/google-chrome
 
+#NVIDIA
+sudo pacman -S bumblebee mesa xf86-video-intel lib32-virtualgl  --noconfirm
+
+
+cd ~/AUR
+git clone https://aur.archlinux.org/nvidia-390xx-utils.git
+cd nvidia-390xx-utils
+sudo makepkg -si --noconfirm
+cd ~/AUR
+sudo rm -r cd ~/AUR/nvidia-390xx-utils
+
+git clone https://aur.archlinux.org/lib32-nvidia-390xx-utils.git
+cd lib32-nvidia-390xx-utils
+sudo makepkg -si --noconfirm
+cd ~/AUR
+sudo rm -r cd ~/AUR/lib32-nvidia-390xx-utils
+
+git clone https://aur.archlinux.org/nvidia-390xx.git
+cd nvidia-390xx
+sudo makepkg -si --noconfirm
+cd ~/AUR
+sudo rm -r cd ~/AUR/nvidia-390xx
+
+
+gpasswd -a technomag31 bumblebee
+systemctl enable bumblebeed.service
 
 
 cd
@@ -42,7 +70,7 @@ wget https://github.com/Technomag31/my1/raw/main/attach/arch_logo.png
 sudo mv -f ~/downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
 
 echo 'add core modules'
-sed -i 's/MODULES=()/MODULES=(crc32c libcrc32 zlib_deflate btrfs)/g' /
+sed -i 's/MODULES=()/MODULES=(crc32c libcrc32 zlib_deflate btrfs nvidia nvidia_modset nvidia_uvm nvidia_drm)/g' /
 sudo mkinitcpio -p linux-zen 
 
 sudo pacman -Syu
